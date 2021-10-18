6ea18bbece2d0b5ed6fe73c90285209d8b84dc9c
Daniel Mensing <daniel.mensing16@gmail.com>
2021-10-18 16:15:06 +0200

    Package structure

--- python_utility_functions/__init__.py
+++ python_utility_functions/__init__.py
@@ -1,6 +1,6 @@
 name = 'python_utility_functions'
 
-from image_utilities import *
-from visualization_utilities import *
-from python_utilities import *
-from path_utilities import *
+from .image_utilities import *
+from .visualization_utilities import *
+from .python_utilities import *
+from .path_utilities import *
